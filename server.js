24a81e5e9f56f401ee5ac499ef106b66db85368f
Muhammad Omair <omairzamir5@gmail.com>
2020-06-05 12:43:51 +0500

    express, http modules added

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
-const io = require('socket.io')(3000)
-const users = []
+const socketIO = require('socket.io');
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const app = express();
+
+//Initialize SOcket.io Socket
+const server = http.createServer(app);
+const io = socketIO(server);
+
+// //Port
+const port = process.env.PORT || 3000;
+const users = {}
+
+// const publicPath = path.join(__dirname + "/../public");
+// app.use(express.static(publicPath));
 
 io.on('connection', socket => {
+    
     socket.on('new-user', name => {
         users[socket.id] = name
         socket.broadcast.emit('user-connected', name)   
@@ -15,3 +30,12 @@ io.on('connection', socket => {
         delete users[socket.id]
     })
 })
+app.get('/', (req, res)=>{
+    
+    res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(port, ()=> {
+    console.log('Server Listening on Port: ' + port);
+    
+});
